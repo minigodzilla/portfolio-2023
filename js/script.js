e04353e4d68e223a587525a42c0b50965d6d2832
Steve Diabo <135646014+stevediabo-ndconnect@users.noreply.github.com>
2023-07-06 22:13:00 -0400

    prototyping a UI

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const data = {
       content: "Lorem ipsum",
     },
     {
-      name: "Workshops &amp; Education",
+      name: "Workshops + Education",
       slug: "workshops-and-education",
       content: "Lorem ipsum",
     },
@@ -27,24 +27,35 @@ const body = document.querySelector("body");
 const nav = document.querySelector("nav");
 
 function navigateTo(i) {
-  // window.history.pushState = '/' + data.pages[i].slug;
-  // Current URL: https://my-website.com/page_a
   const nextURL = "/" + data.pages[i].slug;
   const nextTitle = "";
   const nextState = {};
 
-  // This will create a new entry in the browser's history, without reloading
   window.history.pushState(nextState, nextTitle, nextURL);
-  body.className = "step-" + i;
+  body.className = "navigated step-" + i;
+}
+
+function navigateToHome() {
+  const nextURL = "/";
+  const nextTitle = "";
+  const nextState = {};
+
+  window.history.pushState(nextState, nextTitle, nextURL);
+  body.className = "";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  body.className = "step-0";
-  for (let i = 0; i < data.pages.length; i++) {
+  for (let i = 1; i < data.pages.length; i++) {
     let div = document.createElement("div");
     let span = document.createElement("span");
-    div.setAttribute("onclick", "navigateTo(" + i + ")");
+    let backBtn = document.createElement("span");
+    span.setAttribute("onclick", "navigateTo(" + i + ")");
+    backBtn.setAttribute("onclick", "navigateToHome()");
+    span.classList.add("label");
+    backBtn.classList.add("back-btn");
+    span.innerHTML = data.pages[i].name;
     nav.appendChild(div).classList.add("nav-item");
-    div.appendChild(span).innerHTML = data.pages[i].name;
+    div.appendChild(backBtn);
+    div.appendChild(span);
   }
 });
