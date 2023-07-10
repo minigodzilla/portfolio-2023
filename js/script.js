9d84d0d452f070a1b7b26a07d46442f569491c1b
Steve Diabo <135646014+stevediabo-ndconnect@users.noreply.github.com>
2023-07-10 00:51:19 -0400

    good js-generated index pages

--- js/script.js
+++ js/script.js
@@ -13,25 +13,51 @@ const data = {
             "vintage tech",
             "indigenous",
           ],
-          content: "Lorem ipsum",
+          content:
+            "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos itaque labore optio nulla qui laboriosam, reprehenderit a deserunt! Eius nesciunt fugit ad neque assumenda doloribus voluptatum vitae praesentium dolorem.</p>",
+        },
+        "dancing-light-lanterns": {
+          name: "Dancing Light Lanterns",
+          year: "2022",
+          tags: ["microcontroller", "indigenous"],
+          content:
+            "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos itaque labore optio nulla qui laboriosam, reprehenderit a deserunt! Eius nesciunt fugit ad neque assumenda doloribus voluptatum vitae praesentium dolorem.</p>",
+        },
+        "indigital-arcade": {
+          name: "inDigital Arcade",
+          year: "2021",
+          tags: ["retro gaming", "indigenous"],
+          content:
+            "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos itaque labore optio nulla qui laboriosam, reprehenderit a deserunt! Eius nesciunt fugit ad neque assumenda doloribus voluptatum vitae praesentium dolorem.</p>",
+        },
+        "trivia-phone": {
+          name: "Trivia Phone",
+          year: "2021",
+          tags: ["projection", "microcontroller", "vintage tech"],
+          content:
+            "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos itaque labore optio nulla qui laboriosam, reprehenderit a deserunt! Eius nesciunt fugit ad neque assumenda doloribus voluptatum vitae praesentium dolorem.</p>",
         },
       },
     },
     "web-development": {
       name: "Web Development",
-      content: "Lorem ipsum",
+      content:
+        "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos itaque labore optio nulla qui laboriosam, reprehenderit a deserunt! Eius nesciunt fugit ad neque assumenda doloribus voluptatum vitae praesentium dolorem.</p>",
     },
     "game-development": {
       name: "Game Development",
-      content: "Lorem ipsum",
+      content:
+        "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos itaque labore optio nulla qui laboriosam, reprehenderit a deserunt! Eius nesciunt fugit ad neque assumenda doloribus voluptatum vitae praesentium dolorem.</p>",
     },
     "workshops-and-education": {
       name: "Workshops + Education",
-      content: "Lorem ipsum",
+      content:
+        "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos itaque labore optio nulla qui laboriosam, reprehenderit a deserunt! Eius nesciunt fugit ad neque assumenda doloribus voluptatum vitae praesentium dolorem.</p>",
     },
   },
 };
 
+const pages = data.pages;
 const body = document.querySelector("body");
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
@@ -39,7 +65,8 @@ const main = document.querySelector("main");
 let thisPage = "";
 let lastPage = "";
 
-function navigateTo(i) {
+function navigateTo(page) {
+  //   const nextURL = "/" + page;
   const nextURL = "/";
   const nextTitle = "";
   const nextState = {};
@@ -47,7 +74,7 @@ function navigateTo(i) {
   window.history.pushState(nextState, nextTitle, nextURL);
 
   lastPage = thisPage;
-  thisPage = i;
+  thisPage = page;
 
   body.className = "";
   main.className = "";
@@ -60,11 +87,10 @@ function navigateTo(i) {
     main.classList.add("show-lv1");
   }
   main.classList.add("show-" + thisPage);
-
-  console.log("thisPage: " + thisPage + ", lastPage: " + lastPage);
 }
 
 function navigateBack() {
+  //   const nextURL = "/" + lastPage;
   const nextURL = "/";
   const nextTitle = "";
   const nextState = {};
@@ -82,7 +108,6 @@ function navigateBack() {
     main.classList.add("show-" + lastPage);
   }
   main.classList.remove("show-" + thisPage);
-  //   document.querySelector("#" + thisPage).classList.add("visited");
 
   thisPage = lastPage;
   lastPage = "";
@@ -90,23 +115,68 @@ function navigateBack() {
   console.log("thisPage: " + thisPage + ", lastPage: " + lastPage);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  for (const key in data.pages) {
-    if (Object.prototype.hasOwnProperty.call(data.pages, key)) {
-      const { name } = data.pages[key];
+function createIndexPages(pages, container, parent) {
+  for (const pageId in pages) {
+    if (pages.hasOwnProperty(pageId)) {
+      const page = pages[pageId];
+      const div = document.createElement("div");
+      const img = document.createElement("img");
+      const year = document.createElement("div");
+      const label = document.createElement("h2");
+
+      div.className = "hero";
+      year.className = "year";
+      label.className = "label";
+
+      img.setAttribute("src", "assets/" + parent + "/" + pageId + ".jpg");
+      year.innerText = page.year;
+      label.innerHTML = page.name;
+
+      div.appendChild(img);
+      div.appendChild(year);
+      div.appendChild(label);
+      container.appendChild(div);
+    }
+  }
+}
+
+function createPages(pages) {
+  for (const pageId in pages) {
+    if (pages.hasOwnProperty(pageId)) {
+      const page = pages[pageId];
+      const pageName = page.name;
+      const div = document.createElement("div");
+
+      if (page.pages) {
+        div.className = "page page-lv1 " + pageId;
+        createIndexPages(page.pages, div, pageId);
+      } else {
+        div.className = "page page-lv2 " + pageId;
+        div.innerText = pageName;
+      }
+
+      main.appendChild(div);
+    }
+  }
+}
+
+function createNav() {
+  for (const pageId in pages) {
+    if (pages.hasOwnProperty(pageId)) {
+      const page = pages[pageId];
       const div = document.createElement("div");
       const container = document.createElement("div");
       const label = document.createElement("span");
       const backBtn = document.createElement("span");
 
-      label.addEventListener("click", () => navigateTo(key));
+      label.addEventListener("click", () => navigateTo(pageId));
       backBtn.addEventListener("click", navigateBack);
 
       container.classList.add("container");
       label.classList.add("label");
       backBtn.classList.add("back-btn");
 
-      label.innerHTML = name;
+      label.innerText = page.name;
 
       nav.appendChild(div).classList.add("nav-item");
       div.appendChild(container);
@@ -114,6 +184,11 @@ document.addEventListener("DOMContentLoaded", () => {
       container.appendChild(label);
     }
   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  createPages(data.pages);
+  createNav();
 });
 
 const page = document.querySelector("#ancestors-gate");
@@ -121,7 +196,7 @@ const page = document.querySelector("#ancestors-gate");
 page.addEventListener("scroll", function () {
   const distance = page.scrollTop;
   if (distance < 0) distance = 0;
-  document.querySelector(".article video").style.transform = `translateY(${
+  document.querySelector(".hero .img").style.transform = `translateY(${
     distance * 0.5
   }px)`;
 });
@@ -129,7 +204,7 @@ page.addEventListener("scroll", function () {
 window.addEventListener("scroll", function () {
   const distance = window.scrollY;
   if (distance < 0) distance = 0;
-  document.querySelector(".article video").style.transform = `translateY(${
+  document.querySelector(".hero .img").style.transform = `translateY(${
     distance * 0.5
   }px)`;
 });
